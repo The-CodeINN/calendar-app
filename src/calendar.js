8064ac048137e3bc267695df47fbd27cf8ef9409
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-09-01 22:08:34 +0000

    Final piece

--- src/calendar.js
+++ src/calendar.js
@@ -1,39 +1,124 @@
-import { getDay, getDaysInMonth, getMonth, getYear } from "./dates";
-import { state } from "./state";
+import { getDay, getMonth, getYear, getDaysInMonth } from './dates';
+import { showEventModal } from './event-modal';
+import { state } from './state';
 
 const template = `
-    <section class="calendar data-view="${state.calendar.view}">
-        <div class="inner"></div>
-    </section>
-`
+  <section class="calendar" data-view="${state.calendar.view}">
+    <div class="inner">
+    </div>
+  </section>
+`;
 
-export function initCalendar() {
-    state.$element.insertAdjacentHTML("beforeend", template);
-    state.calendar.$element = state.$element.querySelector("section.calendar");
-    updateView();
+function onCalendarClick(ev) {
+  if (ev.target.classList.contains('cell')) {
+    const date = ev.target.getAttribute('data-date');
+    showEventModal(date, (value) => {
+      if (!state.events.hasOwnProperty(date)) {
+        state.events[date] = [];
+      }
+      state.events[date].push(value);
+      updateView();
+    });
+  }
 }
 
-function updateView() { 
-    const { date } = state.calendar;
-    
-    const currentYear = getYear();
-    const currentMonth = getMonth();
-    const currentDay = getDay();
+export function initCalendar() {
+  state.$element.insertAdjacentHTML('beforeend', template);
+  state.calendar.$element = state.$element.querySelector('section.calendar');
+  updateView();
 
-    const year = getYear(date);
-    const month = getMonth(date);
+  state.calendar.$element.addEventListener('click', onCalendarClick);
 
-    const days = getDaysInMonth(year, month);
+  state.calendar.setDate = (date) => {
+    state.calendar.date = date;
+    updateView();
+  };
 
-    let content = "";
-    for (let day = 0; day <= day; day++) {
-        let isCurrentDay = currentYear === year && currentMonth === month && currentDay === day;
+  setupDragDrop();
+}
 
-        content += `
-            <div class="cell ${isCurrentDay ? "act" : ""}"
-                data-date="${year}-${month}-${day}">
-                   <label>${day}</label>
-            </div>
-            `
+function updateView() {
+  const { date } = state.calendar;
+
+  const cYear = getYear();
+  const cMonth = getMonth();
+  const cDay = getDay();
+
+  const year = getYear(date);
+  const month = getMonth(date);
+
+  const days = getDaysInMonth(year, month);
+  let content = '';
+  for (let day = 1; day <= days; day++) {
+    content += `<div 
+    class="cell ${
+      cYear === year && cMonth === month && cDay === day ? 'act' : ''
+    }" 
+    data-date="${year}-${month}-${day}">
+      <label>${day}</label>`;
+
+    const events = state.events[`${year}-${month}-${day}`];
+    if (events) {
+      content += `<ul>
+        ${events.map((it) => `<li class="ev">${it}</li>`).join('')}
+      </ul>`;
     }
+
+    content += `</div>`;
+  }
+  state.calendar.$element.querySelector('.inner').innerHTML = content;
+
+  state.calendar.onDateChange$.publish(date);
+}
+
+let isMoving = false;
+let activeEvent = null;
+let activeCell = null;
+function setupDragDrop() {
+  window.addEventListener('mousedown', (ev) => {
+    if (!ev.target.classList.contains('ev')) return;
+
+    isMoving = true;
+    activeEvent = ev.target;
+    activeEvent.style.width = `${activeEvent.clientWidth}px`;
+    activeEvent.classList.add('moving');
+  });
+
+  window.addEventListener('mouseup', (ev) => {
+    if (!isMoving) return;
+
+    isMoving = false;
+    activeEvent.classList.remove('moving');
+    activeEvent.style.top = '';
+    activeEvent.style.left = '';
+
+    if (activeCell) {
+      const event = activeEvent.innerText;
+      const prevCell = activeEvent.parentNode.parentNode;
+      const key = activeCell.getAttribute('data-date');
+      const prevKey = prevCell.getAttribute('data-date');
+
+      state.events[prevKey] = state.events[prevKey].filter(
+        (it) => it !== event
+      );
+
+      if (!state.events[key]) {
+        state.events[key] = [];
+      }
+      state.events[key].push(event);
+
+      updateView();
+    }
+  });
+
+  window.addEventListener('mousemove', (ev) => {
+    if (!isMoving || !activeEvent) return;
+
+    if (ev.target.classList.contains('cell')) {
+      activeCell = ev.target;
+    }
+
+    activeEvent.style.top = `${ev.clientY}px`;
+    activeEvent.style.left = `${ev.clientX}px`;
+  });
 }
--- src/header.js
+++ src/header.js
@@ -1,46 +1,54 @@
-import { state } from "./state";
+import { getExactDate, addMonths, getMonth, getYear } from './dates';
+import { i18n } from './i18n';
+import { state } from './state';
 
 const template = `
-    <div id="calendar">
-        <header class="main">
-            <a class="logo">📅</a>
-            <button>Today</button>
+<div id="calendar">
+  <header class="main">
+    <a class="logo">📅</a>
+    <button>Today</button>
 
-            <nav>
+    <nav>
+      <button data-dir="prev">←</button>
+      <button data-dir="next">→</button>
+    </nav>
 
-            <button data-dir="prev">←</button>
-            <button data-dir="next">→<button>  
-
-            </nav>
-
-            <h2> September 2022 </h2>
-        </header>
-    </div>
+    <h2>August 2022</h2>
+  </header>
+</div>
 `;
 
 export function initHeader() {
-    state.$element.insertAdjacentHTML("beforeend", template);
+  state.$element.insertAdjacentHTML('beforeend', template);
 
-    const header = document.querySelector("header.main");
-    header.addEventListener("click", ev => {
-        const { target } = ev;
+  const header = document.querySelector('header.main');
+  header.addEventListener('click', (ev) => {
+    const { target } = ev;
 
-        switch (target.tagName) { 
-            case "BUTTON": {
-                const parent = target.parentElement;
-
-                parent.tagName === "NAV" ? OnNavClick(target) : onTodayClick();
-            }
+    switch (target.tagName) {
+      case 'BUTTON':
+        {
+          const parent = target.parentElement;
+          parent.tagName === 'NAV' ? onNavClick(target) : onTodayClick();
         }
-
-        // Todo: Listen for calendar change - update label
-    });
-
-    function OnNavClick(target) {
-
+        break;
     }
+  });
 
-    function onTodayClick() { 
-        
-    }
+  const label = header.querySelector('h2');
+  state.calendar.onDateChange$.subscribe((date) => {
+    const month = getMonth(date);
+    const year = getYear(date);
+    label.innerHTML = `${i18n['month-' + month]} ${year}`;
+  });
+}
+
+function onTodayClick() {
+  state.calendar.setDate(getExactDate());
+}
+
+function onNavClick(target) {
+  const direction = target.getAttribute('data-dir');
+  const next = addMonths(state.calendar.date, direction === 'prev' ? -1 : +1);
+  state.calendar.setDate(next);
 }
