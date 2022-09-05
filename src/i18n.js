387b3f891bc16750929fb0abf35cab6a05e75152
The-CodeINN <67247138+The-CodeINN@users.noreply.github.com>
2022-09-05 15:05:24 +0000

    Corrected an error

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,7 @@ export const i18n = {
   'month-0': 'January',
   'month-1': 'February',
   'month-2': 'March',
-  'month-3': 'Aril',
+  'month-3': 'April',
   'month-4': 'March',
   'month-5': 'June',
   'month-6': 'July',
